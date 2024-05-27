d91a6f4aa4b6091cee589a25ce803708c074a1b4
Akibur Rahman <me.akiburrahman@gmail.com>
2024-05-27 09:02:50 +0600

    update

--- gen-lab-report/src/services/chatService.js
+++ gen-lab-report/src/services/chatService.js
@@ -1,7 +1,8 @@
-// chatService.js
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-async function generateAiResponse(apiKey, prompt) {
+
+
+async function generate(apiKey, prompt) {
     const gemini_api_key = apiKey;
     const googleAI = new GoogleGenerativeAI(gemini_api_key);
 
@@ -13,11 +14,8 @@ async function generateAiResponse(apiKey, prompt) {
         const request = prompt;
         const result = await geminiModel.generateContent(request);
         const response = result.response;
-        return response.text();
+        console.log(response.text());
     } catch (error) {
         console.log("response error", error);
-        throw error;
     }
-}
-
-module.exports = { generateAiResponse };
+};
