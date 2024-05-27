d6391fe2e25f088f29d8a2577394545e8772413d
Akibur Rahman <me.akiburrahman@gmail.com>
2024-05-27 08:04:04 +0600

    dymanic api key functionality added

--- gen-lab-report/src/services/chatService.js
+++ gen-lab-report/src/services/chatService.js
@@ -1,21 +1,21 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const gemini_api_key = "AIzaSyD3zJ_ihXbUbwOmFEmAy0x8PKMeiGokl4Y";
-const googleAI = new GoogleGenerativeAI(gemini_api_key);
 
-const geminiModel = googleAI.getGenerativeModel({
-    model: "gemini-pro",
-});
 
-const generate = async () => {
+async function generate(apiKey, prompt) {
+    const gemini_api_key = apiKey;
+    const googleAI = new GoogleGenerativeAI(gemini_api_key);
+
+    const geminiModel = googleAI.getGenerativeModel({
+        model: "gemini-pro",
+    });
+
     try {
-        const prompt = "Hello, Are you there?";
-        const result = await geminiModel.generateContent(prompt);
+        const request = prompt;
+        const result = await geminiModel.generateContent(request);
         const response = result.response;
         console.log(response.text());
     } catch (error) {
         console.log("response error", error);
     }
 };
-
-generate();
