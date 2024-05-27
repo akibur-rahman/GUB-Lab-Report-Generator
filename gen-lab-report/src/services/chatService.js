dd0476ccc7569ba526413a79dcbe11b89f7689a0
Akibur Rahman <me.akiburrahman@gmail.com>
2024-05-27 14:04:58 +0600

    fill edit scraped due to errors

--- gen-lab-report/src/services/chatService.js
+++ gen-lab-report/src/services/chatService.js
@@ -6,7 +6,9 @@ async function generateAiResponse(apiKey, prompt) {
     console.log(prompt);
     const gemini_api_key = apiKey;
     const googleAI = new GoogleGenerativeAI(gemini_api_key);
+
     const geminiModel = googleAI.getGenerativeModel({ model: "gemini-pro" });
+
 
     try {
         const contextualPrompt = `${conversationHistory
